9ba478d7355865cc61b70a9dc959550e54765c88
MonkeyLovers69 <137757068+MonkeyLovers69@users.noreply.github.com>
2023-07-09 00:02:55 +0500

    Add files via upload

---
+++ main (14).py
@@ -0,0 +1,17 @@
+print('hello!')
+answer1 = input()
+if answer1 == 'hello':
+    print('its nice to meet you! how u feel?')
+else:
+    print('dont be mean!say hello!')
+answer2 = input()
+if answer2 == 'i am good':
+    print('thats nice to hear!')
+elif answer2 == 'i am bad':
+    print('whats wrong?you can tell me')
+else:
+    print('ok')
+answer3 = input()
+print('i am just dumb program but i wish youll be alright')
+
+
